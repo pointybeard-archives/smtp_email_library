cfe08772b21f3be1e8cd1e0a7ebdec737d20a9d0
Alistair Kearney <alistair@21degrees.com.au>
2010-06-24 11:09:52 +1000

    Fixed emailing

--- lib/class.email.php
+++ lib/class.email.php
@@ -18,17 +18,17 @@
 		}
 		
 		public function send(){
-			
+					
 			$headers = array(
 				'Message-ID'	=> sprintf('<%s@%s>', md5(uniqid(time())), $_SERVER['SERVER_NAME']),
-				'Return-Path'	=> $this->_vars['from'],
+				'Return-Path'	=> 'noreply@' . URL,
 				'Importance'	=> 'normal',
 				'Priority'		=> 'normal',
 				'X-Sender'		=> 'Symphony Email Module <noreply@symphony-cms.com>',
 				'X-Mailer'		=> 'Symphony Email Module',
 				'X-Priority'	=> '3',
 				'MIME-Version'	=> '1.0',
-				'Content-Type'	=> 'text/plain; charset=UTF-8',						
+				'Content-Type'	=> 'text/plain; charset=UTF-8',
 				'To'		 	=> $this->_vars['to'], 
 				'From' 			=> $this->_vars['from'], 
 			 	'Reply-To'		=> $this->_vars['from'],
@@ -38,8 +38,19 @@
 			foreach($this->_vars['headers'] as $name => $value){
 				$headers[$name] = $value;
 			}
+
+			$credentials = Symphony::Configuration()->get('smtp_email_library');
 			
-			Mail::factory('smtp', array(Symphony::Configuration()->get('smtp_email_library')))->send(
+			$smtp = Mail::factory('smtp', array(
+				'host' => $credentials['host'],
+				'port' => $credentials['port'],
+				'auth' => ((int)$credentials['auth'] == 1 ? true : false),
+				'username' => $credentials['username'],
+				'password' => $credentials['password'],
+				'debug' => ((int)$credentials['debug'] == 1 ? true : false),
+			));
+			
+			$smtp->send(
 				$this->_vars['to'], 
 				$headers, 
 				$this->_vars['message']
@@ -48,7 +59,7 @@
 			if(PEAR::isError($mail)){
 				throw new Exception($mail->getMessage());
 			}
-				
+			
 			return true;
 		}		
 		
