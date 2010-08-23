bd9801811d07817be7b8a236d7bdda5fbaf1fe65
Alistair <hi@alistairkearney.com>
2010-08-23 14:29:14 +1000

    Updated to work with Symphony 2.2 core email api

--- extension.driver.php
+++ extension.driver.php
@@ -1,18 +1,16 @@
 <?php
 
-	require_once(dirname(__FILE__) . '/lib/class.email.php');
-
 	Class extension_SMTP_Email_Library extends Extension{
 		
 		const HTACCESS_PEAR_INCLUDE = "## EMAIL EXTENSION PEAR LIBRARY\nphp_value include_path  \"extensions/smtp_email_library/lib/pear:.\"";
 		
 		public function about(){
 			return array('name' => 'SMTP Email Library',
-						 'version' => '1.1',
-						 'release-date' => '2010-02-12',
+						 'version' => '2.0',
+						 'release-date' => '2010-08-23',
 						 'author' => array('name' => 'Alistair Kearney',
-										   'website' => 'http://www.pointybeard.com',
-										   'email' => 'alistair@symphony-cms.com')
+										   'website' => 'http://alistairkearney.com',
+										   'email' => 'hi@alistairkearney.com')
 				 		);
 		}
 		
@@ -30,41 +28,12 @@
 					'callback' => 'cbSavePreferences'
 				),
 					
-				array(
-					'page' => '/blueprints/events/new/',
-					'delegate' => 'AppendEventFilter',
-					'callback' => 'cbAddFilterToEventEditor'
-				),
-
-				array(
-					'page' => '/blueprints/events/edit/',
-					'delegate' => 'AppendEventFilter',
-					'callback' => 'cbAddFilterToEventEditor'
-				),	
-				
-				array(
-					'page' => '/blueprints/events/new/',
-					'delegate' => 'AppendEventFilterDocumentation',
-					'callback' => 'cbAppendEventFilterDocumentation'
-				),
-
-				array(
-					'page' => '/blueprints/events/edit/',
-					'delegate' => 'AppendEventFilterDocumentation',
-					'callback' => 'cbAppendEventFilterDocumentation'
-				),				
-					
-				array(
-					'page' => '/frontend/',
-					'delegate' => 'EventPostSaveFilter',
-					'callback' => 'cbSendEmailSMTPFilter'
-				),	
 			);
 		}
 				
 		public function uninstall(){
 			self::__update_htaccess(true);
-			Symphony::Configuration()->remove('smtp_email_library');			
+			Symphony::Configuration()->remove('smtp_email_library');
 			$this->_Parent->saveConfig();
 		}
 			
@@ -116,7 +85,7 @@
 		}
 		
 		public function enable(){
-			return self::__update_htaccess();			
+			return self::__update_htaccess();
 		}
 
 		public function disable(){
@@ -138,150 +107,21 @@
 			
 			if($removing == false){
 				$htaccess = preg_replace(
-					'/### Symphony 2\.0\.x ###\n*/i', 
-					"### Symphony 2.0.x ###\n\n" . self::HTACCESS_PEAR_INCLUDE . "\n\n", 
+					'/### Symphony 2(\.\d)?\.x ###\n*/i', 
+					"### Symphony 2.x ###\n\n" . self::HTACCESS_PEAR_INCLUDE . "\n\n", 
 					$htaccess
 				);
 			}
 			else{
 				//clean up the extra new line characters
 				$htaccess = preg_replace(
-					'/### Symphony 2\.0\.x ###\n*/i', 
-					"### Symphony 2.0.x ###\n", 
+					'/### Symphony 2(\.\d)?\.x ###\n*/i', 
+					"### Symphony 2.x ###\n", 
 					$htaccess
-				);				
+				);
 			}
 			
 			return @file_put_contents(DOCROOT . '/.htaccess', $htaccess);
 		}
-
-		private function __sendEmailFindFormValue($needle, $haystack, $discard_field_name=true, $default=NULL, $collapse=true){
-
-			if(preg_match('/^(fields\[[^\]]+\],?)+$/i', $needle)){
-				$parts = preg_split('/\,/i', $needle, -1, PREG_SPLIT_NO_EMPTY);
-				$parts = array_map('trim', $parts);
-
-				$stack = array();
-				foreach($parts as $p){ 
-					$field = str_replace(array('fields[', ']'), '', $p);
-					($discard_field_name ? $stack[] = $haystack[$field] : $stack[$field] = $haystack[$field]);
-				}
-
-				if(is_array($stack) && !empty($stack)) return ($collapse ? implode(' ', $stack) : $stack);
-				else $needle = NULL;
-			}
-
-			$needle = trim($needle);
-			if(empty($needle)) return $default;
-
-			return $needle;
-
-		}
-		
-		public function cbAppendEventFilterDocumentation(array $context=array()){
-			if(!@in_array('smtp-email-library-send-email-filter', $context['selected'])) return;
-			
-			$context['documentation'][] = new XMLElement('h3', __('Send Email via Direct SMTP Connection'));
-
-			$context['documentation'][] = new XMLElement('p', __('The send email filter, upon the event successfully saving the entry, takes input from the form and send an email to the desired recipient. <b>This filter currently does not work with the "Allow Multiple" option.</b> The following are the recognised fields:'));
-
-			$context['documentation'][] = contentBlueprintsEvents::processDocumentationCode(
-				'send-email[sender-email] // '.__('Optional').self::CRLF.
-				'send-email[sender-name] // '.__('Optional').self::CRLF.						
-				'send-email[subject] // '.__('Optional').self::CRLF.
-				'send-email[body]'.self::CRLF.
-				'send-email[recipient] // '.__('comma separated list of author usernames.'));
-
-			$context['documentation'][] = new XMLElement('p', __('All of these fields can be set dynamically using the exact field name of another field in the form as shown below in the example form:'));
-
-	        $context['documentation'][] = contentBlueprintsEvents::processDocumentationCode('<form action="" method="post">
-	<fieldset>
-		<label>'.__('Name').' <input type="text" name="fields[author]" value="" /></label>
-		<label>'.__('Email').' <input type="text" name="fields[email]" value="" /></label>
-		<label>'.__('Message').' <textarea name="fields[message]" rows="5" cols="21"></textarea></label>
-		<input name="send-email[sender-email]" value="fields[email]" type="hidden" />
-		<input name="send-email[sender-name]" value="fields[author]" type="hidden" />		
-		<input name="send-email[subject]" value="You are being contacted" type="hidden" />
-		<input name="send-email[body]" value="fields[message]" type="hidden" />
-		<input name="send-email[recipient]" value="fred" type="hidden" />
-		<input id="submit" type="submit" name="action[save-contact-form]" value="Send" />
-	</fieldset>
-</form>');
-		}
-		
-		public function cbSendEmailSMTPFilter(array $context=array()){
-			
-			if(!@in_array('smtp-email-library-send-email-filter', $context['event']->eParamFILTERS)) return;
-
-			$fields = $_POST['send-email'];
-			
-			$fields['recipient'] = $this->__sendEmailFindFormValue($fields['recipient'], $_POST['fields'], true);
-			$fields['recipient'] = preg_split('/\,/i', $fields['recipient'], -1, PREG_SPLIT_NO_EMPTY);
-			$fields['recipient'] = array_map('trim', $fields['recipient']);
-
-			$fields['recipient'] = Symphony::Database()->fetch("SELECT `email`, CONCAT(`first_name`, ' ', `last_name`) AS `name` FROM `tbl_authors` WHERE `username` IN ('".@implode("', '", $fields['recipient'])."') ");
-
-			$fields['subject'] = $this->__sendEmailFindFormValue($fields['subject'], $context['fields'], true, __('[Symphony] A new entry was created on %s', array(Symphony::Configuration()->get('sitename', 'general'))));
-			$fields['body'] = $this->__sendEmailFindFormValue($fields['body'], $context['fields'], false, NULL, false);
-			$fields['sender-email'] = $this->__sendEmailFindFormValue($fields['sender-email'], $context['fields'], true, 'noreply@' . parse_url(URL, PHP_URL_HOST));
-			$fields['sender-name'] = $this->__sendEmailFindFormValue($fields['sender-name'], $context['fields'], true, 'Symphony');
-			$fields['from'] = $this->__sendEmailFindFormValue($fields['from'], $context['fields'], true, $fields['sender-email']);		
-						
-			$section = Symphony::Database()->fetchRow(0, "SELECT * FROM `tbl_sections` WHERE `id` = ".$context['event']->getSource()." LIMIT 1");
-			
-			$edit_link = URL.'/symphony/publish/'.$section['handle'].'/edit/'.$context['entry_id'].'/';
-
-			$body = __('Dear <!-- RECIPIENT NAME -->,') . General::CRLF . General::CRLF . __('This is a courtesy email to notify you that an entry was created on the %1$s section. You can edit the entry by going to: %2$s', array($section['name'], $edit_link)). General::CRLF . General::CRLF;
-
-			if(is_array($fields['body'])){
-				foreach($fields['body'] as $field_handle => $value){
-					$body .= "=== $field_handle ===" . General::CRLF . General::CRLF . $value . General::CRLF . General::CRLF;
-				}
-			}
-
-			else $body .= $fields['body'];
-
-			$errors = array();
-
-			if(!is_array($fields['recipient']) || empty($fields['recipient'])){
-				$context['messages'][] = array('smtp-email-library-send-email-filter', false, __('No valid recipients found. Check send-email[recipient] field.'));
-			}
-
-			else{
-				
-				foreach($fields['recipient'] as $r){
-					
-					$email = new LibraryEmail;
-
-					$email->to = vsprintf('%2$s <%1$s>', array_values($r));
-					$email->from = sprintf('%s <%s>', $fields['sender-name'], $fields['sender-email']);
-					$email->subject = $fields['subject'];
-					$email->message = str_replace('<!-- RECIPIENT NAME -->', $r['name'], $body);
-					$email->setHeader('Reply-To', $fields['from']);
-
-					try{
-						$email->send();
-					}
-					catch(Exception $e){
-						$errors[] = $email;
-					}
-
-				}
-
-				if(!empty($errors)){
-					$context['messages'][] = array('smtp-email-library-send-email-filter', false, 'The following email addresses were problematic: ' . General::sanitize(implode(', ', $errors)));
-				}
-
-				else $context['messages'][] = array('smtp-email-library-send-email-filter', true);
-			}
-		}
-
-		public function cbAddFilterToEventEditor($context){
-			$context['options'][] = array(
-				'smtp-email-library-send-email-filter', @in_array('smtp-email-library-send-email-filter', $context['selected']), 'Send Email via Direct SMTP Connection'
-			);			
-			
-		}
-	
 	}
 
--- lib/class.email.php
+++ lib/class.email.php
@@ -1,27 +1,19 @@
 <?php
 
-	Class LibraryEmail{
-		
-		private $_vars;
-		
+	Class PEARSMTP_Email extends Email{
+	
 		public function __construct(){
-			require_once "pear/Mail.php";
-			$this->_vars = array('headers' => array());
+			require_once "Mail.php";
+			parent::__construct();
 		}
-		
-		public function __set($name, $value){
-			$this->_vars[$name] = $value;
-		}
-		
-		public function setHeader($name, $value){
-			$this->_vars['headers'][$name] = $value;
-		}
-		
+	
 		public function send(){
-					
+		
+			$this->validate();
+
 			$headers = array(
 				'Message-ID'	=> sprintf('<%s@%s>', md5(uniqid(time())), $_SERVER['SERVER_NAME']),
-				'Return-Path'	=> 'noreply@' . DOMAIN,
+				'Return-Path'	=> $this->sender_email_address,
 				'Importance'	=> 'normal',
 				'Priority'		=> 'normal',
 				'X-Sender'		=> 'Symphony Email Module <noreply@symphony-cms.com>',
@@ -29,18 +21,18 @@
 				'X-Priority'	=> '3',
 				'MIME-Version'	=> '1.0',
 				'Content-Type'	=> 'text/plain; charset=UTF-8',
-				'To'		 	=> $this->_vars['to'], 
-				'From' 			=> $this->_vars['from'], 
-			 	'Reply-To'		=> $this->_vars['from'],
-				'Subject' 		=> $this->_vars['subject']
+				'To'		 	=> $this->recipient,
+				'From'			=> "{$this->sender_name} <{$this->sender_email_address}>",
+		 		'Reply-To'		=> $this->sender_email_address,
+				'Subject' 		=> $this->subject
 			);
-			
-			foreach($this->_vars['headers'] as $name => $value){
+		
+			foreach($this->headers as $name => $value){
 				$headers[$name] = $value;
 			}
 
 			$credentials = Symphony::Configuration()->get('smtp_email_library');
-			
+		
 			$smtp = Mail::factory('smtp', array(
 				'host' => $credentials['host'],
 				'port' => $credentials['port'],
@@ -49,19 +41,19 @@
 				'password' => $credentials['password'],
 				'debug' => ((int)$credentials['debug'] == 1 ? true : false),
 			));
-			
+		
 			$mail = $smtp->send(
-				$this->_vars['to'], 
+				$this->recipient, 
 				$headers, 
-				$this->_vars['message']
+				$this->message
 			);
-			
+
 			if(PEAR::isError($mail)){
 				throw new Exception($mail->getMessage());
 			}
-			
-			return true;
-		}		
 		
+			return true;
+		}	
 	}
 	
+	return 'PEARSMTP_Email';
